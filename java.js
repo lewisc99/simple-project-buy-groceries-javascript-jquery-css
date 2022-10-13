35b2a20567cdcec30f3421a9b4653b752acf2a93
lewisc99 <lewiscontato99@gmail.com>
2022-10-13 15:26:58 -0300

    added input to add new groceries and remove groceries

--- java.js
+++ java.js
@@ -5,7 +5,32 @@ $(function() {
   var $inputValue = $("#itemDescription");
   var $counter = $("#counter");
   var $ulList = $("#list-group");
+  var countId  = 0;
   
+  $ulList.on("click",function(e)
+  {
+
+    if (e.target.className == "complete")
+    {
+      $ulList.find("#" + e.target.id).animate({
+        opacity:0.0,
+        paddingLeft: '+=80'
+      },500, function()
+      {
+        $(this).remove();
+      })
+    }
+ 
+    var newItem =  $ulList.find("#" + e.target.id);
+    console.log(newItem);
+    newItem.removeClass("hot");
+    newItem.addClass("complete");
+    $ulList.find("#" + e.target.id).remove();
+    $ulList.find('li:last').after(newItem);
+
+  })
+
+
 
   $formDiv.hide();
 
@@ -17,8 +42,9 @@ $(function() {
 
   $add.on('click',function()
   {
+    countId += 1;
     var newText = $inputValue.val();
-    $('li:last').after('<li class="">' + newText + '</li>');
+    $('li:last').after(`<li class="hot" id="newItem${countId}">  ${newText} </li>`);
     let valores = $ulList.children().length;
    $counter.text("");
    $counter.text(valores);
